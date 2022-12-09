68c4735bc21093ab9ebdfaedd98a9989329e3c8a
FerrariRoma <win9592@gmail.com>
2022-12-09 17:57:41 +0900

    Update : User domain 구현 완료
    Service와 Controller에 로직과 라우터 추가

--- my-cv/src/users/users.controller.ts
+++ my-cv/src/users/users.controller.ts
@@ -1,12 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private UsersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() { email, password }: CreateUserDto) {
-    this.UsersService.create(email, password);
+    this.usersService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: number, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body);
   }
 }
