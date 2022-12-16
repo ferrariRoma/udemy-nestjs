34ce0170bf75bceb3a9e667bd1cc117b9c3a996e
FerrariRoma <win9592@gmail.com>
2022-12-16 13:12:43 +0900

    Update : validation pipe와 cookie session을 app module로 장착

--- my-cv/src/main.ts
+++ my-cv/src/main.ts
@@ -1,22 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['490ghb65dmu0'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
