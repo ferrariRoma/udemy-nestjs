033bb59766da2a62915241036db2ec59a5e39c80
FerrariRoma <win9592@gmail.com>
2022-12-18 11:54:19 +0900

    Update : create report dto 작성

--- my-cv/src/reports/dtos/create-report.dto.ts
+++ my-cv/src/reports/dtos/create-report.dto.ts
@@ -1,7 +1,15 @@
-import { IsNumber, IsString } from 'class-validator';
+import {
+  IsNumber,
+  IsString,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
 
 export class CreateReportDto {
   @IsNumber()
+  @Min(1000000)
   price: number;
 
   @IsString()
@@ -11,14 +19,18 @@ export class CreateReportDto {
   model: string;
 
   @IsNumber()
+  @Min(1960)
+  @Max(2040)
   year: number;
 
-  @IsNumber()
+  @IsLongitude()
   lng: number;
 
-  @IsNumber()
+  @IsLatitude()
   lat: number;
 
   @IsNumber()
+  @Min(0)
+  @Max(10000000)
   mileage: number;
 }
