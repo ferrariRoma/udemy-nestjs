d7a657308c133714ac8b8ef11b0944c276371ecb
FerrariRoma <win9592@gmail.com>
2022-12-19 16:42:46 +0900

    Update : post, patch request router 추가

--- my-cv/src/reports/reports.controller.ts
+++ my-cv/src/reports/reports.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Param,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptor/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(
+    @Param('id') id: number,
+    @Body() { approved }: ApproveReportDto,
+  ) {
+    return this.reportsService.changeApproval(id, approved);
+  }
+}
